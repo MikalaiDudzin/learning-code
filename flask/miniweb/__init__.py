5b1954978f638118bd2be3b29326042203631a1b
MikalaiDudzin <89949328+MikalaiDudzin@users.noreply.github.com>
2021-12-10 02:24:40 +0300

    Add files via upload

--- flask/miniweb/__init__.py
+++ flask/miniweb/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, flash, session, redirect, abort , g
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort, g, make_response
 import sqlite3
 import os
 
@@ -13,10 +13,12 @@ app.config.from_object(__name__)
 
 app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
 
+
 def connect_db():
     conn = sqlite3.connect(app.config['DATABASE'])
     conn.row_factory = sqlite3.Row
     return conn
+
 
 def create_db():
     db = connect_db()
@@ -25,33 +27,41 @@ def create_db():
     db.commit()
     db.close()
 
+
 def get_db():
-    if not hasattr(g , 'link_db'):
+    if not hasattr(g, 'link_db'):
         g.link_db = connect_db()
     return g.link_db
 
-menu = [{'name': 'Установка', 'url': 'install-flask'},
-        {'name': 'Первое приложение', 'url': 'first-app'},
-        {'name': 'Обратная связь', 'url': 'contact'}
-        ]
-#
-#
-@app.route('/')
-def index():
+
+# menu = [{'name': 'Установка', 'url': 'install-flask'},
+#         {'name': 'Первое приложение', 'url': 'first-app'},
+#         {'name': 'Обратная связь', 'url': 'contact'}
+#         ]
+
+dbase = None
+
+
+@app.before_request
+def before_request():
+    global dbase
     db = get_db()
     dbase = FDataBase(db)
+
+
+@app.route('/')
+def index():
     return render_template('index.html', menu=dbase.getMenu(), posts=dbase.getPostsAnonce())
+
 
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g, 'link_db'):
         g.link_db.close()
 
+
 @app.route('/add_post', methods=['POST', 'GET'])
 def addPost():
-    db = get_db()
-    dbase = FDataBase(db)
-
     if request.method == 'POST':
         if len(request.form['name']) > 4 and len(request.form['post']) > 10:
             res = dbase.addPost(request.form['name'], request.form['post'], request.form['url'])
@@ -61,17 +71,25 @@ def addPost():
                 flash('Статья добавлена успешно', category='success')
         else:
             flash('Ошибка добавления статьи', category='error')
-    return render_template('add_post.html',menu=dbase.getMenu(), title='Добавить статью')
+    return render_template('add_post.html', menu=dbase.getMenu(), title='Добавить статью')
+
 
 @app.route('/post/<alias>')
 def showPost(alias):
-    db = get_db()
-    dbase = FDataBase(db)
     title, post = dbase.getPost(alias)
     if not title:
         abort(404)
     return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
 
+
+@app.route('/login')
+def login():
+    return render_template('login.html', menu=dbase.getMenu(), title='Авторизация')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html', menu=dbase.getMenu(), title='Регистрация')
 
 
 # @app.route('/about')
@@ -109,6 +127,24 @@ def showPost(alias):
 #         return redirect(url_for('profile', username=session['userLogged']))
 #     return render_template('login.html', title='Авторизация', menu=menu)
 #
+
+# cookies
+# @app.route('/login')
+# def login():
+#     log = ' '
+#     if request.cookies.get('logged'):
+#         log = request.cookies.get('logged')
+#
+#     res = make_response(f'<h1>Форма аторизации</h1><p>logged:{log}')
+#     res.set_cookie('logged', 'yes', 30*24*3600)
+#     return res
+#
+# @app.route('/logout')
+# def logout():
+#     res = make_response('<p>Вы больше не авторизированны! </p> ')
+#     res.set_cookie('logged', '', 0)
+#     return res
+
 #
 # @app.route('/profile/<username>')
 # def profile(username):
