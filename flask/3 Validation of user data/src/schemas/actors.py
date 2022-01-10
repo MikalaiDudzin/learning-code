33f6e0be4e5bb59e4071476274603ba99e0284db
MikalaiDudzin <89949328+MikalaiDudzin@users.noreply.github.com>
2022-01-10 22:47:52 +0300

    Delete flask/3 Validation of user data directory

--- flask/3 Validation of user data/src/schemas/actors.py
+++
@@ -1,9 +0,0 @@
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
-
-from src.database.models import Actor
-
-
-class ActorSchema(SQLAlchemyAutoSchema):
-    class Mata:
-        model = Actor
-        load_instance = True
